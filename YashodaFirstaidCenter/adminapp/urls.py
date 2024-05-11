b885fdafb8ef6d8e05e12ea9280fd1bce249ae4d
Annepuuday111 <116910543+Annepuuday111@users.noreply.github.com>
2024-05-11 16:54:57 +0530

    V3.0.0.1
    commit

--- YashodaFirstaidCenter/adminapp/urls.py
+++ YashodaFirstaidCenter/adminapp/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('blog/', views.blog, name='blog'),
     path('contact/', views.contact, name='contact'),
     path('services/', views.services, name='services'),
     path('login/', views.login, name='login'),
